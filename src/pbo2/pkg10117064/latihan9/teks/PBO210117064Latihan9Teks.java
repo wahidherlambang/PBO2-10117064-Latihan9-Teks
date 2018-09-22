cf1761ebefcb0de73b0dca32a0537d21fd86a335
wahidherlambang <43497169+wahidherlambang@users.noreply.github.com>
2018-09-22 23:23:47 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117064/latihan9/teks/PBO210117064Latihan9Teks.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117064.latihan9.teks;
+
+/**
+ *
+ * @author WAHID HERLAMBANG
+ * NAMA     : Wahid Herlambang Suroso
+ * KELAS    : PBO2
+ * NIM      : 10117064
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * teks
+ */
+public class PBO210117064Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char" + tab + "berisi" + c);
+    }
+    
+}
